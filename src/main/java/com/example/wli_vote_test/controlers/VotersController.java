3fe57642c50a3b6855d2e01f954bff89bf2ab32f
WojciechLitw <66257050+WojciechLitw@users.noreply.github.com>
2024-02-13 09:16:34 +0100

    Update VotersController.java
    chore: added context with comments

--- src/main/java/com/example/wli_vote_test/controlers/VotersController.java
+++ src/main/java/com/example/wli_vote_test/controlers/VotersController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+*    Voter endpoint handler.
+*/
 @RestController
 public class VotersController {
 
